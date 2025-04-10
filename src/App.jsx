f39d174a130adb5a74a6381928cf0604148a1eb7
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-10 19:32:55 +0200

    feat: add css

--- src/App.jsx
+++ src/App.jsx
@@ -35,31 +35,35 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>Movies List</h1>
-      </div>
-
-      <section>
-        <h2>Cerca Films</h2>
-        <label>Cerca per genere</label>
+      <div className='container'>
         <div>
-          <select value={searchGenre} onChange={(e => setSearchGenre(e.target.value))}>
-            <option value=''>---</option>
-            <option>Fantascienza</option>
-            <option>Thriller</option>
-            <option>Romantico</option>
-            <option>Azione</option>
-          </select>
+          <h1>Movies List</h1>
         </div>
-      </section>
 
-      <label>Cerca per titolo</label>
-      <div><input type="text" value={searchTitle} onChange={(e => setSearchTitle(e.target.value))} /></div>
+        <section>
+          <h2>Cerca Films</h2>
+          <label>Cerca per genere</label>
+          <div>
+            <select value={searchGenre} onChange={(e => setSearchGenre(e.target.value))}>
+              <option value=''>---</option>
+              <option>Fantascienza</option>
+              <option>Thriller</option>
+              <option>Romantico</option>
+              <option>Azione</option>
+            </select>
+          </div>
+        </section>
 
-      {moviesList.map((movie, i) => <section key={i}>
-        <h4>{movie.title}</h4>
-        <p>{movie.genre}</p>
-      </section>)}
+        <section>
+          <label>Cerca per titolo</label>
+          <div><input type="text" value={searchTitle} onChange={(e => setSearchTitle(e.target.value))} /></div>
+        </section>
+
+        {moviesList.map((movie, i) => <section key={i}>
+          <h4>{movie.title}</h4>
+          <p>{movie.genre}</p>
+        </section>)}
+      </div>
     </>
   )
 }
