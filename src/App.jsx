c615b7a0a2f3f2ea631a9275d414e2ab15acf2fb
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-10 17:10:52 +0200

    feat: add movies filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import movies from './data/movies.'
 
 function App() {
 
+  const [moviesList, setMoviesList] = useState(movies);
+  const [searchGenre, setSearchGenre] = useState('');
+
+  useEffect(() => {
+
+    let results = movies;
+
+
+    if (searchGenre !== '') {
+      results = movies.filter(movie => movie.genre.includes(searchGenre))
+
+    } else {
+
+    }
+    setMoviesList(results)
+
+  }, [searchGenre])
 
   return (
     <>
       <div>
         <h1>Movies List</h1>
       </div>
+
+      <section>
+        <h2>Cerca Films</h2>
+        <label>Cerca per genere</label>
+        <div>
+          <select value={searchGenre} onChange={(e => setSearchGenre(e.target.value))}>
+            <option>---</option>
+            <option>Fantascienza</option>
+            <option>Thriller</option>
+            <option>Romantico</option>
+            <option>Azione</option>
+          </select>
+        </div>
+      </section>
+
+
       {movies.map((movie, i) => <section key={i}>
         <h4>{movie.title}</h4>
         <p>{movie.genre}</p>
